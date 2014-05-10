fdd8c42141e60dbc46bee5d4ede8ad23fec3dbb6
Jesse La Russo <jlarusso@canvas.is>
2014-05-09 22:18:53 -0400

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YouAreInARoom::Application.config.secret_key_base = 'a4eac3a0e64846a981bf6bd80ff7b9a66f9bc37bd2b0e803836de032fca523c9c1619683e52ef2517401c6c92d4cc053fc27c4f812a3d9e01b711b65778b3c41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YouAreInARoom::Application.config.session_store :cookie_store, key: '_you-are-in-a-room_session'
