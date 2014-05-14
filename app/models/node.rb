e9fc2ae9dcfb8e34185dc6a563d962804aec141a
Jesse La Russo <jlarusso@canvas.is>
2014-05-14 14:42:52 -0400

    Seed some nodes/edges

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,13 @@
 class Node < ActiveRecord::Base
-  has_many :from_links, foreign_key: "to_id", class_name: "Edge"
-  has_many :from_nodes, through: :from_links
+  has_many :from_edges, foreign_key: "to_id", class_name: "Edge"
+  has_many :from_nodes, through: :from_edges
 
-  has_many :to_links, foreign_key: "from_id", class_name: "Edge"
-  has_many :to_nodes, through: :to_links
+  has_many :to_edges, foreign_key: "from_id", class_name: "Edge"
+  has_many :to_nodes, through: :to_edges
+
+  def connect(node)
+    to_nodes << node
+    from_nodes << node
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
-Node.create(name: 'Back of the room')
-Node.create(name: 'Front of the room')
-Node.create(name: 'Elevators')
-Node.create(name: 'Service elevator')
-Node.create(name: 'Kitchen')
-Node.create(name: 'Phone booth')
+Node.destroy_all
+Edge.destroy_all
+
+n1 = Node.create(name: 'Back of the room')
+n2 = Node.create(name: 'Front of the room')
+n3 = Node.create(name: 'Elevators')
+n4 = Node.create(name: 'Service elevator')
+n5 = Node.create(name: 'Kitchen')
+n6 = Node.create(name: 'Phone booth')
+
+n1.connect n2
+n1.connect n4
+n2.connect n5
+n2.connect n3
+n3.connect n6
+
