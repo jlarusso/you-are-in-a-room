c1b58c15aff16a99dcde7cde4f1c25cb8f320bab
Jesse La Russo <jlarusso@canvas.is>
2014-05-24 17:59:26 -0400

    Can verb on subjects that are local

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ActiveRecord::Base
-  include Food
-
   belongs_to :owner, polymorphic: true
 
   acts_as_savable :owner_id, :owner_type
+
+  # Return an item or nil
+  def self.find_local(name)
+    items = Player.first.items + Player.first.node.items
+    items.find { |item| item.name == name }
+  end
+
+  # Return text if legal action or nil otherwise
+  def action(verb)
+    if verb == 'look'
+      description
+    else
+      binding.pry
+    end
+  end
 end
--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,12 +1,32 @@
 class Parser
   def self.parse(str)
     node = Player.first.node
+    tokens = str.to_s.split(' ')
 
-    case str
-    when 'look'
-      [node.description, node.surroundings]
+    # No command
+    if tokens.empty?
+      ["Please enter a command."]
+    elsif tokens.size == 1
+      token = tokens[0]
+
+      # Plain look
+      if token == 'look'
+        [node.description, node.surroundings]
+      else
+        ["That doesn't make sense."]
+      end
     else
-      ["That doesn't make sense."]
+      verb = tokens.shift
+      subject = tokens.join(' ')
+      item = Item.find_local(subject)
+
+      # Action on an item or node
+      if item
+        legal_action = item.action(verb)
+        legal_action ? [legal_action] : ["You can't do that"]
+      else
+        ["You don't see that here"]
+      end
     end
   end
 end
